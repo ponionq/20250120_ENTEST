3f68f8fe66c1eeeeacdcfb92ea58dfb1b3829cdd
ponionq <ponionq@gmail.com>
2025-01-20 15:10:47 +0900

    초기 파일 셋팅

---
+++ components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <NuxtLink to="/">HOME</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/">ABOUT</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/">CONTACT</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>index.vue</div>
+</template>
+<script setup>
+useSeoMeta({
+    title: "20250120_ENTEST",
+    description: "20250120_ENTEST 테스트 페이지 입니다. ",
+    ogTitle: "20250120_ENTEST",
+    ogDescription: "[og:description]",
+    ogImage: "[og:image]",
+    ogUrl: "[og:url]",
+});
+useHead({
+    link: [
+        {
+            rel: "icon",
+            type: "image/png",
+            href: "/images/favicon.png",
+        },
+    ],
+});
+</script>
